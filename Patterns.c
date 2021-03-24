8e7f854a3805cdce989aab7a159e7e28515e829b
brycepatt24 <81180644+brycepatt24@users.noreply.github.com>
2021-03-24 10:05:58 -0400

    Add files via upload

---
+++ Patterns.c
@@ -0,0 +1,797 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char shape;
+	int x;
+
+
+
+	printf("What shape do you want to draw?\n");
+	scanf_s("%c", &shape);
+
+	printf("Write your input with size of shape you want next to the letter of the shape you want. \n");
+	scanf_s("%d", &x);
+	printf("\n");
+
+	if (shape == 'L')
+	{
+		if (x == 3 && shape == 'L')
+			for (int x = 0; x < 1; x++)
+			{
+				printf("*");
+
+				printf("\n");
+
+				for (int x = 0; x < 1; x++)
+				{
+					printf("**");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("***");
+					printf("\n");
+				}
+
+			}
+		if (x == 4 && shape == 'L')
+			for (int x = 0; x < 1; x++)
+			{
+				printf("*");
+
+				printf("\n");
+
+				for (int x = 0; x < 1; x++)
+				{
+					printf("**");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("***");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("****");
+					printf("\n");
+				}
+
+			}
+		if (x == 5 && shape == 'L')
+			for (int x = 0; x < 1; x++)
+			{
+				printf("*");
+
+				printf("\n");
+
+				for (int x = 0; x < 1; x++)
+				{
+					printf("**");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("***");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("****");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("*****");
+					printf("\n");
+				}
+			}
+		if (x == 6 && shape == 'L')
+			for (int x = 0; x < 1; x++)
+			{
+				printf("*");
+
+				printf("\n");
+
+				for (int x = 0; x < 1; x++)
+				{
+					printf("**");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("***");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("****");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("*****");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("******");
+					printf("\n");
+				}
+			}
+		if (x == 7 && shape == 'L')
+			for (int x = 0; x < 1; x++)
+			{
+				printf("*");
+
+				printf("\n");
+
+				for (int x = 0; x < 1; x++)
+				{
+					printf("**");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("***");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("****");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("*****");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("******");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("*******");
+					printf("\n");
+				}
+			}
+		if (x == 8 && shape == 'L')
+			for (int x = 0; x < 1; x++)
+			{
+				printf("*");
+
+				printf("\n");
+
+				for (int x = 0; x < 1; x++)
+				{
+					printf("**");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("***");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("****");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("*****");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("******");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("*******");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("********");
+					printf("\n");
+				}
+			}
+	}
+
+	if (shape == 'R')
+	{
+		if (x == 3 && shape == 'R')
+			for (int x = 0; x < 1; x++)
+			{
+				printf("  *");
+
+				printf("\n");
+
+				for (int x = 0; x < 1; x++)
+				{
+					printf(" **");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("***");
+					printf("\n");
+				}
+
+			}
+		if (x == 4 && shape == 'R')
+			for (int x = 0; x < 1; x++)
+			{
+				printf("   *");
+
+				printf("\n");
+
+				for (int x = 0; x < 1; x++)
+				{
+					printf("  **");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf(" ***");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("****");
+					printf("\n");
+				}
+
+			}
+		if (x == 5 && shape == 'R')
+			for (int x = 0; x < 1; x++)
+			{
+				printf("    *");
+				printf("\n");
+
+				for (int x = 0; x < 1; x++)
+				{
+					printf("   **");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("  ***");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf(" ****");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("*****");
+					printf("\n");
+				}
+			}
+		if (x == 6 && shape == 'R')
+			for (int x = 0; x < 1; x++)
+			{
+				printf("     *");
+
+				printf("\n");
+
+				for (int x = 0; x < 1; x++)
+				{
+					printf("    **");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("   ***");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("  ****");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf(" *****");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("******");
+					printf("\n");
+				}
+			}
+		if (x == 7 && shape == 'R')
+			for (int x = 0; x < 1; x++)
+			{
+				printf("       *");
+
+				printf("\n");
+
+				for (int x = 0; x < 1; x++)
+				{
+					printf("     **");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("    ***");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("   ****");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("  *****");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf(" ******");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("*******");
+					printf("\n");
+				}
+			}
+		if (x == 8 && shape == 'R')
+			for (int x = 0; x < 1; x++)
+			{
+				printf("       *");
+
+				printf("\n");
+
+				for (int x = 0; x < 1; x++)
+				{
+					printf("      **");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("     ***");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("    ****");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("   *****");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("  ******");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf(" *******");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("********");
+					printf("\n");
+				}
+			}
+	}
+
+	if (shape == 'S')
+	{
+		if (x == 3 && shape == 'S')
+			for (int x = 0; x < 1; x++)
+			{
+				printf("***");
+
+				printf("\n");
+
+				for (int x = 0; x < 1; x++)
+				{
+					printf("***");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("***");
+					printf("\n");
+				}
+
+			}
+		if (x == 4 && shape == 'S')
+			for (int x = 0; x < 1; x++)
+			{
+				printf("****");
+
+				printf("\n");
+
+				for (int x = 0; x < 1; x++)
+				{
+					printf("****");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("****");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("****");
+					printf("\n");
+				}
+
+			}
+		if (x == 5 && shape == 'S')
+			for (int x = 0; x < 1; x++)
+			{
+				printf("*****");
+
+				printf("\n");
+
+				for (int x = 0; x < 1; x++)
+				{
+					printf("*****");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("*****");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("*****");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("*****");
+					printf("\n");
+				}
+			}
+		if (x == 6 && shape == 'S')
+			for (int x = 0; x < 1; x++)
+			{
+				printf("******");
+
+				printf("\n");
+
+				for (int x = 0; x < 1; x++)
+				{
+					printf("******");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("******");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("******");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("******");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("******");
+					printf("\n");
+				}
+			}
+		if (x == 7 && shape == 'S')
+			for (int x = 0; x < 1; x++)
+			{
+				printf("*******");
+
+				printf("\n");
+
+				for (int x = 0; x < 1; x++)
+				{
+					printf("*******");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("*******");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("*******");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("*******");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("*******");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("*******");
+					printf("\n");
+				}
+			}
+		if (x == 8 && shape == 'S')
+			for (int x = 0; x < 1; x++)
+			{
+				printf("********");
+
+				printf("\n");
+
+				for (int x = 0; x < 1; x++)
+				{
+					printf("********");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("********");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("********");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("********");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("********");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("********");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("********");
+					printf("\n");
+				}
+			}
+	}
+	if (shape == 'C')
+	{
+		if (x == 3 && shape == 'C')
+			for (int x = 0; x < 1; x++)
+			{
+				printf("  *  *");
+
+				printf("\n");
+
+				for (int x = 0; x < 1; x++)
+				{
+					printf(" **  **");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("***  ***");
+					printf("\n");
+				}
+
+			}
+		if (x == 4 && shape == 'C')
+			for (int x = 0; x < 1; x++)
+			{
+				printf("   *  *");
+
+				printf("\n");
+
+				for (int x = 0; x < 1; x++)
+				{
+					printf("  **  **");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf(" ***  ***");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("****  *****");
+					printf("\n");
+				}
+
+			}
+		if (x == 5 && shape == 'C')
+			for (int x = 0; x < 1; x++)
+			{
+				printf("    *  *");
+
+				printf("\n");
+
+				for (int x = 0; x < 1; x++)
+				{
+					printf("   **  **");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("  ***  ***");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf(" ****  ****");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("*****  *****");
+					printf("\n");
+				}
+			}
+		if (x == 6 && shape == 'C')
+			for (int x = 0; x < 1; x++)
+			{
+				printf("     *  *");
+
+				printf("\n");
+
+				for (int x = 0; x < 1; x++)
+				{
+					printf("    **  **");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("   ***  ***");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("  ****  ****");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf(" *****  *****");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("******  ******");
+					printf("\n");
+				}
+			}
+		if (x == 7 && shape == 'C')
+			for (int x = 0; x < 1; x++)
+			{
+				printf("      *  *");
+
+				printf("\n");
+
+				for (int x = 0; x < 1; x++)
+				{
+					printf("     **  **");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("    ***  ***");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("   ****  ****");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("  *****  *****");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf(" ******  ******");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("******* *******");
+					printf("\n");
+				}
+			}
+		if (x == 8 && shape == 'C')
+			for (int x = 0; x < 1; x++)
+			{
+				printf("       *  *");
+
+				printf("\n");
+
+				for (int x = 0; x < 1; x++)
+				{
+					printf("      **  **");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("     ***  ***");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("    ****  ****");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("   *****  *****");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("  ******  ******");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf(" *******  *******");
+					printf("\n");
+				}
+				for (int x = 0; x < 1; x++)
+				{
+					printf("********  ********");
+					printf("\n");
+				}
+			}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
+
+
